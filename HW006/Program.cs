9fcdd36a86a12b4c67a9cd906d0fc8f6ddb68e34
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-02-13 21:04:35 +0500

    perhabs a working program has been created HW006

---
+++ HW006/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("enter a number");
+int N = int.Parse(Console.ReadLine()??"0");
+int curr = 1;
+int prod = 1;
+void PrintCurrentNumber (int N)
+{
+    prod*=curr;
+    curr++;
+    if(curr<=N)
+    PrintCurrentNumber(N);
+}
+
+PrintCurrentNumber(N);
+Console.Write($"{prod} ");
