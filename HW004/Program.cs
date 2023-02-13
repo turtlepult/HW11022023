d45584884535393432a8f43b46434e68cd8f345b
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-02-13 19:16:36 +0500

    perhabs a working program has been created HW004

---
+++ HW004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+Console.WriteLine("enter a number 1: ");
+int M = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("enter a number 2: ");
+int N = int.Parse(Console.ReadLine()??"0");
+int curr = Math.Min(N,M);
+int max = Math.Max(N,M);
+int sum = 0;
+void PrintCurrentNumber ( int N, int M)
+{
+    sum+=curr;
+    curr++;
+    if(curr<=max)
+    PrintCurrentNumber(N,M);
+}
+
+PrintCurrentNumber(N,M);
+Console.Write($"summ elements in interval ({M} - {N}) = {sum}");
