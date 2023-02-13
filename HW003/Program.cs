a1ad38f61510d7d00329c88152cc81a081cbd9c8
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-02-13 19:10:44 +0500

    perhabs a working program has been created HW002

---
+++ HW003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+Console.WriteLine("enter a number 1: ");
+int M = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("enter a number 2: ");
+int N = int.Parse(Console.ReadLine()??"0");
+int curr = Math.Min(N,M);
+int max = Math.Max(N,M);
+void PrintCurrentNumber ( int N, int M)
+{
+    Console.Write($"{curr} ");
+    curr++;
+    if(curr<=max)
+    PrintCurrentNumber(N,M);
+}
+
+PrintCurrentNumber(N,M);
