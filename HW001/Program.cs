686133f98f41aa11dbee9b7fac3ca9d16a48cc63
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-02-13 19:03:29 +0500

    perhabs a working program has been created HW001

---
+++ HW001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+Console.WriteLine("enter a number");
+int N = int.Parse(Console.ReadLine()??"0");
+int curr = 1;
+void PrintCurrentNumber ( int N)
+{
+    Console.Write($"{curr} ");
+    curr++;
+    if(curr<=N)
+    PrintCurrentNumber(N);
+}
+
+PrintCurrentNumber(N);
