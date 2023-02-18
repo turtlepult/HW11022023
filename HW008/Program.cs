ed587922616e19b1db922507308f4ca0ac43993c
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-02-18 16:10:56 +0500

    perhabs a working programm has been created

--- HW008/Program.cs
+++ HW008/Program.cs
@@ -1,29 +1,23 @@
 ﻿
 // Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
-int ValueEnter(string message)
+int GetNumber(string message)
 {
-    Console.WriteLine(message);
-    return int.Parse(Console.ReadLine()??"0");
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
-int p = 0;
-int FakeFibbo(int first, int second, int n, int p)
-{ 
-    if (n==n)
+void PrintNumber(int numberFirst, int numberSecond, int score)
+{
+    int temporary = 0;
+    if (score > 0)
     {
-        Console.Write($"{first} {second}");
-        n--;
+        Console.Write(numberFirst + numberSecond + " ");
+        temporary = numberFirst;
+        numberFirst =numberSecond;
+        numberSecond += temporary;
+        PrintNumber(numberFirst, numberSecond, score -= 1);
     }
-    else if(n>0)
-    {
-        p = (n-1)+(n-2);
-        Console.Write($"{p} ");
-        n--;
-        FakeFibbo(first,second,n,p);
-    }
-return n;
 }
-
-int first = ValueEnter("enter a first number");
-int second = ValueEnter("enter a second number");
-int n = ValueEnter("enter the number of digits");
-FakeFibbo(first,second,n,p);
+int numberFirst = GetNumber("Введите число M: ");
+int numberSecond = GetNumber("Введите число N: ");
+int score = GetNumber("Введите количество раз, сколько раз мы выведем число: ");
+PrintNumber(numberFirst, numberSecond, score);
