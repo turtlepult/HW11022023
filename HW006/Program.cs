fd28ae789d853cd49748f697e839ad9ea50b5ca0
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-02-18 16:09:06 +0500

    perhabs a working programm has been created

--- HW006/Program.cs
+++ HW006/Program.cs
@@ -3,7 +3,7 @@ int FunAcc(int n, int m)
 {
     while (n != 0)
     {
-        if (m = 0)
+        if (m == 0)
         {
             m /= 1;
         }
@@ -13,8 +13,7 @@ int FunAcc(int n, int m)
         }
     n /= n - 1;
     }
-    
     return m + 1;
 }
-int xxx = FunAcc(2, 4);
+int xxx = FunAcc(1, 2);
 Console.WriteLine(xxx);
---
+++ HW008/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+int ValueEnter(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+int p = 0;
+int FakeFibbo(int first, int second, int n, int p)
+{ 
+    if (n==n)
+    {
+        Console.Write($"{first} {second}");
+        n--;
+    }
+    else if(n>0)
+    {
+        p = (n-1)+(n-2);
+        Console.Write($"{p} ");
+        n--;
+        FakeFibbo(first,second,n,p);
+    }
+return n;
+}
+
+int first = ValueEnter("enter a first number");
+int second = ValueEnter("enter a second number");
+int n = ValueEnter("enter the number of digits");
+FakeFibbo(first,second,n,p);
